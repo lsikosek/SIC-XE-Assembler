0a4de4b60a5be84cf40cb2e4e4b7e63c32ec2da7
Lovro Sikošek <67958862+lsikosek@users.noreply.github.com>
2025-01-04 11:15:39 +0100

    safety commit

--- src/sic/asm/code/Code.java
+++ src/sic/asm/code/Code.java
@@ -5,11 +5,48 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 /**
  * Podporni razred za predmet Sistemska programska oprema.
  * @author jure
  */
 public class Code {
 
-	// TODO
+	private String name;
+	private List<Node> program;
+	private Map<String, Integer> symbols;
+	
+	public Code() {
+		this.name = "";
+		this.program = new ArrayList<>();
+		this.symbols = new HashMap<>();
+
+	}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Node> getProgram() {
+        return program;
+    }
+
+    public void setProgram(List<Node> program) {
+        this.program = program;
+    }
+
+    public Map<String, Integer> getSymbols() {
+        return symbols;
+    }
+
+    public void setSymbols(Map<String, Integer> symbols) {
+        this.symbols = symbols;
+    }
+	
+	
+
 }
---
+++ src/sic/asm/code/InstructionF2.java
@@ -0,0 +1,14 @@
+package sic.asm.code;
+
+import sic.asm.mnemonics.Mnemonic;
+
+public class InstructionF2 extends Node {
+    
+    public InstructionF2(Mnemonic mnemonic) {
+        super(mnemonic);
+    }
+
+    
+
+
+}
